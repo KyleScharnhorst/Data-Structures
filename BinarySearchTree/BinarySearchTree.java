60a89cc440686ba17e603e9d0ccdec7125b8f7c5
KyleScharnhorst <scharkr1@uw.edu>
2017-09-16 03:30:19 -0700

    Add: inorder, preorder, and postorder prints.
    In-order traversal displays the values in the BST in sorted order.
    Pre-order traversal can be used to copy a BST as a given node can be
    created and its children would then be attached.
    Post-order traversal can be used to free an entire BST as parent elements
    are freed last, allowing no nodes to be abandoned.

--- BinarySearchTree/BinarySearchTree.java
+++ BinarySearchTree/BinarySearchTree.java
@@ -3,7 +3,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     public TreeNode<T> insert(T data) {
         if(_head == null) {
-            _head = new TreeNode<T>(data);
+            _head = new TreeNode<>(data);
             return _head;
         }
         return insert(data, _head);
@@ -15,13 +15,13 @@ public class BinarySearchTree<T extends Comparable<T>> {
         int comparison = data.compareTo(node._data);
         if(comparison < 0) {
             if(node._left_node == null) {
-                return (node._left_node = new TreeNode<T>(data));
+                return (node._left_node = new TreeNode<>(data));
             } else {
                 return insert(data, node._left_node);
             }
         } else if (comparison > 0) {
             if (node._right_node == null) {
-                return (node._right_node = new TreeNode<T>(data));
+                return (node._right_node = new TreeNode<>(data));
             } else {
                 return insert(data, node._right_node);
             }
@@ -37,9 +37,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
     private T find(T data, TreeNode<T> node) {
         T result = null;
 
-        if(node == null) {
-            result = null;
-        } else {
+        if(node != null) {
             int comparison = data.compareTo(node._data);
             if(comparison < 0) {
                 result = find(data, node._left_node);
@@ -68,10 +66,61 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
+    public void printInOrder() {
+        System.out.print("InOrder: ");
+        printInOrder(this._head);
+        System.out.println();
+    }
+
+    private void printInOrder(TreeNode<T> node) {
+        if(node != null) {
+            printInOrder(node._left_node);
+            System.out.print(node._data + " ");
+            printInOrder(node._right_node);
+        }
+    }
+
+    public void printPreOrder() {
+        System.out.print("PreOrder: ");
+        printPreOrder(this._head);
+        System.out.println();
+    }
+
+    private void printPreOrder(TreeNode<T> node) {
+        if(node != null) {
+            System.out.print(node._data + " ");
+            printPreOrder(node._left_node);
+            printPreOrder(node._right_node);
+        }
+    }
+
+    public void printPostOrder() {
+        System.out.print("PostOrder: ");
+        printPostOrder(this._head);
+        System.out.println();
+    }
+
+    private void printPostOrder(TreeNode<T> node) {
+        if(node != null) {
+            printPostOrder(node._left_node);
+            printPostOrder(node._right_node);
+            System.out.print(node._data + " ");
+        }
+    }
+
     public static void main(String[] args) {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
         System.out.println("Running...");
-        // assertNotNull(bst.insert(5));
+        /* BST structure
+         *                      5
+         *                   2      6
+         *                              7
+         *                                  12
+         *                               11
+         *                            8
+         *                               9
+         *                                  10
+         */
         assert bst.insert(5) != null;
         assert bst._head != null;
         assert bst.insert(5) == null;
@@ -85,5 +134,8 @@ public class BinarySearchTree<T extends Comparable<T>> {
         assert bst.insert(10) != null;
         System.out.println("Find result: " + bst.find(10));
         System.out.println(bst._head);
+        bst.printInOrder();
+        bst.printPreOrder();
+        bst.printPostOrder();
     }
 }
