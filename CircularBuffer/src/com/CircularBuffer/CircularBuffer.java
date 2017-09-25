7cb58f2c94ad813462daeea4f68a1119aece087e
KyleScharnhorst <scharkr1@uw.edu>
2017-09-24 17:50:26 -0700

    Add: initial file for circular buffer.

---
+++ CircularBuffer/src/com/CircularBuffer/CircularBuffer.java
@@ -0,0 +1,26 @@
+package com.CircularBuffer;
+
+public class CircularBuffer {
+    private char[] _buffer;
+    public final int _buffer_size;
+
+    public CircularBuffer(int buffer_size) {
+        this._buffer_size = buffer_size;
+    }
+
+    public char[] readOutBuffer() {
+
+    }
+
+    public boolean writeToBuffer(char c) {
+
+    }
+
+    public static void main(String[] args) {
+        //create circular buffer
+
+        //create threads that read and write the buffer.
+
+        //
+    }
+}
