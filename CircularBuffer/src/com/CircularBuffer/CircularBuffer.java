334cea39b3f5ca4393ea13343b6d0ce4ec43fb53
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-09-24 19:57:07 -0700

    Add: basic stubs and worker threads.

--- CircularBuffer/src/com/CircularBuffer/CircularBuffer.java
+++ CircularBuffer/src/com/CircularBuffer/CircularBuffer.java
@@ -9,18 +9,48 @@ public class CircularBuffer {
     }
 
     public char[] readOutBuffer() {
-
+        return new char[1];
     }
 
     public boolean writeToBuffer(char c) {
-
+        return false;
     }
 
-    public static void main(String[] args) {
+    private static class TestWriteWorker implements Runnable {
+        CircularBuffer _buffer;
+        public TestWriteWorker(CircularBuffer cb) {
+            this._buffer = cb;
+        }
+        public void run() {
+
+        }
+    }
+
+    private static class TestReadWorker implements Runnable {
+        CircularBuffer _buffer;
+        public TestReadWorker(CircularBuffer cb) {
+            this._buffer = cb;
+        }
+        public void run() {
+
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         //create circular buffer
+        CircularBuffer cb = new CircularBuffer(100);
 
         //create threads that read and write the buffer.
+        Thread write_thread = new Thread(new TestWriteWorker(cb));
+        write_thread.start();
+        Thread read_thread = new Thread(new TestReadWorker(cb));
+        read_thread.start();
 
-        //
+        //wait some amount of time
+        Thread.sleep(10000);
+
+        //interupt threads and exit
+        write_thread.interrupt();
+        read_thread.interrupt();
     }
 }
