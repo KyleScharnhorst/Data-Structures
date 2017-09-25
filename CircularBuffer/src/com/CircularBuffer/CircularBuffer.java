bd45dbf2bdaa22c37964ce5b1f245ffd93fd4382
Kyle Scharnhorst <kyle.scharnhorst@gmail.com>
2017-09-24 23:28:02 -0700

    Add: read write ptrs and force size to be power of 2.
    needs to be a power of two for when one of the pointers overflows. When
    the overflow happens we need to be pointing at the zero index and prior
    to the overflow we need to point to the last index. A buffer size of a
    power of 2 enforces this.

--- CircularBuffer/src/com/CircularBuffer/CircularBuffer.java
+++ CircularBuffer/src/com/CircularBuffer/CircularBuffer.java
@@ -5,12 +5,31 @@ import java.util.Random;
 public class CircularBuffer {
     private char[] _buffer;
     public final int _buffer_size;
+    private int _write_index = 0;
+    private int _read_index = 0;
 
     public CircularBuffer(int buffer_size) {
+        if(!IsPowerOfTwo(buffer_size)) {
+            throw new IllegalArgumentException();
+        }
         this._buffer_size = buffer_size;
+        _buffer = new char[buffer_size];
+    }
+
+    private boolean IsPowerOfTwo(int i) {
+        return (i & (i - 1)) == 0;
+    }
+
+//    private int GetReadableDataRange() {
+//
+//    }
+
+    private int getTrueIndex(int i) {
+        return i % _buffer_size;
     }
 
     public char[] readOutBuffer() {
+        //char result = _buffer[];
         return new char[1];
     }
 
@@ -49,14 +68,18 @@ public class CircularBuffer {
         public TestReadWorker(CircularBuffer cb) {
             this._buffer = cb;
         }
-        public void run() {
 
+        public void run() {
+            while(!Thread.interrupted()) {
+
+            }
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        int buffer_size = 64;
         //create circular buffer
-        CircularBuffer cb = new CircularBuffer(100);
+        CircularBuffer cb = new CircularBuffer(buffer_size);
 
         //create threads that read and write the buffer.
         Thread write_thread = new Thread(new TestWriteWorker(cb));
@@ -67,7 +90,7 @@ public class CircularBuffer {
         //wait some amount of time
         Thread.sleep(10000);
 
-        //interupt threads and exit
+        //interrupt threads and exit
         write_thread.interrupt();
         read_thread.interrupt();
     }
